e65b5585c5ef16f463ffb799f2a955ef434a544b
younghokim <yh.kim@purple.io>
2020-11-04 18:15:08 +0900

    utterances 추가

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,18 +1,28 @@
-import markdownStyles from './markdown-styles.module.css'
+import markdownStyles from "./markdown-styles.module.css";
 
 type Props = {
-  content: string
-}
+  content: string;
+};
 
 const PostBody = ({ content }: Props) => {
   return (
-    <div className="max-w-2xl mx-auto">
-      <div
-        className={markdownStyles['markdown']}
-        dangerouslySetInnerHTML={{ __html: content }}
+    <>
+      <div className="max-w-2xl mx-auto">
+        <div
+          className={markdownStyles["markdown"]}
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
+      </div>
+      <script
+        src="https://utteranc.es/client.js"
+        repo="0ho0ho/0ho0ho.github.io"
+        issue-term="pathname"
+        theme="github-light"
+        crossOrigin="anonymous"
+        async
       />
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default PostBody
+export default PostBody;
