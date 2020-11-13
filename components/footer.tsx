f0649769fba33daa1837f885eb28bb4eed12acc7
younghokim <yh.kim@purple.io>
2020-11-13 18:08:54 +0900

    github 주석

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <footer className="text-lg text-teal-600 text-center py-12">
-      <a href="https://github.com/0ho0ho" target="_blank">
+      {/* <a href="https://github.com/0ho0ho" target="_blank">
         github
-      </a>
+      </a> */}
     </footer>
   );
 };
