ed18fe57d49463e52556956d97daceb7031cf805
oungo <y2k6273@naver.com>
2023-09-26 20:41:02 +0900

    remove: remove post title

--- src/components/post-header.tsx
+++ src/components/post-header.tsx
@@ -1,5 +1,4 @@
 import DateFormatter from './date-formatter';
-import PostTitle from './post-title';
 
 interface Props {
   title: string;
@@ -9,7 +8,9 @@ interface Props {
 const PostHeader = ({ title, date }: Props) => {
   return (
     <>
-      <PostTitle>{title}</PostTitle>
+      <h1 className="max-w-2xl mx-auto mb-3 text-4xl font-bold leading-tight tracking-tighter lg:text-3xl md:leading-none">
+        {title}
+      </h1>
       <div className="max-w-2xl mx-auto">
         <div className="mb-6 text-gray-700">
           <DateFormatter dateString={date} />
