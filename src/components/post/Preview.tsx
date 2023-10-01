a792d02276f9c572eb4843efd0949b9c9a5189a7
oungo <y2k6273@naver.com>
2023-10-01 11:13:37 +0900

    design: add transform scale to post preview

--- src/components/post/Preview.tsx
+++ src/components/post/Preview.tsx
@@ -11,7 +11,10 @@ interface Props {
 
 const Preview = ({ title, date, excerpt, slug }: Props) => {
   return (
-    <Link href={`/posts/${slug}`} className="flex flex-col gap-2 p-4 border border-gray-400 rounded-md">
+    <Link
+      href={`/posts/${slug}`}
+      className="flex flex-col gap-2 p-4 duration-200 transform border border-gray-300 rounded-md hover:scale-101"
+    >
       <h2 className="text-xl font-semibold text-teal-600">{title}</h2>
       <p className="text-gray-700">{excerpt}</p>
       <div className="text-gray-700">
