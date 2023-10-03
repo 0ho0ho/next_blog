2ab5e2a3d23565b7568a646a308df34cd25164e3
oungo <y2k6273@naver.com>
2023-10-03 15:36:17 +0900

    design: change main color

--- src/components/post/Preview.tsx
+++ src/components/post/Preview.tsx
@@ -15,7 +15,7 @@ const Preview = ({ title, date, excerpt, slug }: Props) => {
       href={`/posts/${slug}`}
       className="flex flex-col gap-2 p-4 duration-200 transform border border-gray-300 rounded-md hover:scale-101"
     >
-      <h2 className="text-xl font-semibold text-teal-600">{title}</h2>
+      <h2 className="text-xl font-semibold text-pink-600">{title}</h2>
       <p className="text-gray-700">{excerpt}</p>
       <div className="text-gray-700">
         <DateFormatter dateString={date} />
