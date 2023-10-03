fe1cdbf581eadf6f32b698426683c846a5be8d54
oungo <y2k6273@naver.com>
2023-10-03 17:28:57 +0900

    feat: add gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
+import Script from 'next/script';
+
 import Layout from '@components/layout';
+import * as gtag from '@lib/gtag';
 
 import type { Metadata } from 'next';
 import type { PropsWithChildren } from 'react';
+
 import '@styles/index.css';
 
 export const metadata: Metadata = {
@@ -41,6 +45,28 @@ const RootLayout = ({ children }: PropsWithChildren) => {
     <html lang="en">
       <body>
         <Layout>{children}</Layout>
+        {process.env.NODE_ENV === 'production' && (
+          <>
+            <Script
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+            />
+            <Script
+              id="gtag-init"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${gtag.GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+              }}
+            />
+          </>
+        )}
       </body>
     </html>
   );
