06c0476db630053f3f022937b590605cd0343a98
oungo <y2k6273@naver.com>
2023-10-03 16:13:05 +0900

    feat: add metadata for index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,32 @@ import { getAllPosts } from '@lib/api';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Home',
-  description: 'Welcome to Next.js',
+  title: 'oungo',
+  description: '개발을 즐기는 프론트엔드 개발자입니다. 아무 글이나 끄적이고 있습니다.',
+  metadataBase: new URL('https://oungo.github.io'),
+  alternates: {
+    canonical: '/',
+  },
+  openGraph: {
+    url: 'https://oungo.github.io',
+    title: 'oungo',
+    description: '개발을 즐기는 프론트엔드 개발자입니다. 아무 글이나 끄적이고 있습니다.',
+    images: [
+      {
+        url: '/og-image.svg',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary',
+    title: 'oungo',
+    description: '개발을 즐기는 프론트엔드 개발자입니다. 아무 글이나 끄적이고 있습니다.',
+    images: {
+      url: 'https://oungo.github.io/og-image.svg',
+    },
+  },
 };
 
 const Page = () => {
